f4f601288e1e7a64b5419cfbd6f4b62310117b40
Javier Calvarro Nelson <jacalvar@microsoft.com>
2018-06-11 18:49:22 -0700

    TestingSample

---
+++ test/TestingSample.Tests/BasicTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestingSample.Tests
+{
+    public class BasicTests : IClassFixture<CustomWebApplicationFactory>
+    {
+        public BasicTests(CustomWebApplicationFactory factory)
+        {
+            Factory = factory;
+        }
+
+        public CustomWebApplicationFactory Factory { get; }
+
+        [Fact]
+        public async Task GetHome()
+        {
+            // Arrange
+            var client = Factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ test/TestingSample.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingSample.Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            var value = Environment.GetEnvironmentVariable("MY_CONTENT_ROOT");
+            if (!string.IsNullOrEmpty(value))
+            {
+                builder.UseContentRoot(value);
+            }
+        }
+    }
+}
